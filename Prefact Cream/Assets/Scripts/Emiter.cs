2acf4b4c373b2a69576295137d48a9c361ed404e
mynkrthd <31612057+mynkrthd@users.noreply.github.com>
2021-03-14 11:39:31 +0530

    POC
    Project Setup and POC

---
+++ Prefact Cream/Assets/Scripts/Emiter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emiter : MonoBehaviour
+{
+    public GameObject Cream;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void InitiateCream()
+    {
+        GameObject.Instantiate(Cream,transform.position,Quaternion.identity);
+    }
+
+    public void StopFlow()
+    {
+        Debug.Log("End Flow");
+       //gameObject.GetComponentInChildren<Rigidbody>().useGravity = true;
+     
+        
+    }
+}
---
+++ Prefact Cream/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    Emiter _emiter;
+    private void Awake()
+    {
+        _emiter = GetComponent<Emiter>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _emiter.InitiateCream();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Flow Continue");
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            _emiter.StopFlow();
+        }
+    }
+}
